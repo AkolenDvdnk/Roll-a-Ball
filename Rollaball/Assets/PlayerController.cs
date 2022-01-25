7efd039378443ad7b0fb8201353a4a03670b24ba
AkolenDvdnk <ak0lenfs@gmail.com>
2022-01-25 12:13:10 +0200

    Moving the Player

---
+++ Rollaball/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private float movementX, movementY;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+}
