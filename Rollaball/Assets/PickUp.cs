2121fd1c3ed51cab2cc1cfb813311ee5739d29f9
AkolenDvdnk <ak0lenfs@gmail.com>
2022-01-25 14:59:50 +0200

    Detecting Collisions with Collectibles

---
+++ Rollaball/Assets/PickUp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private void Update()
+    {
+        Rotate();
+    }   
+    private void Rotate()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
