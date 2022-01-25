f6472d9ffe33f4498d06261b1e0b0b08ecf0fa25
AkolenDvdnk <ak0lenfs@gmail.com>
2022-01-25 16:54:42 +0200

    Made the first mission
    The wall disappears after collecting all the coins

--- Rollaball/Assets/ScoreCounter.cs
+++ Rollaball/Assets/ScoreCounter.cs
@@ -4,11 +4,31 @@ using TMPro;
 public class ScoreCounter : MonoBehaviour
 {
     public static int Score;
+    public static bool Collected = false;
 
     public TextMeshProUGUI scoreUI;
 
+    private int pickUpsCount;
+
+    private void Start()
+    {
+        pickUpsCount = GameObject.FindGameObjectsWithTag("PickUp").Length;
+    }
     private void Update()
     {
-        scoreUI.text = "Score: " + Score;
+        UpdateScoreUI();
+    }
+    private void UpdateScoreUI()
+    {
+        if (Score < pickUpsCount)
+        {
+            scoreUI.text = $"1. Score: {Score} / {pickUpsCount}";
+        }
+        else 
+        {
+            Collected = true;
+            scoreUI.text = $"<s>1. Score: {Score} / {pickUpsCount}<s>";
+            scoreUI.color = new Color32(200, 200, 200, 255);
+        }
     }
 }
---
+++ Rollaball/Assets/Wall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (ScoreCounter.Collected)
+        {
+            animator.SetTrigger("Collected");
+        }
+    }
+}
